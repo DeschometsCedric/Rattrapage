4a81b41bf5fb86b7d31e83d9ae9c8afb63ce1f29
DeschometsCedric <84242014+DeschometsCedric@users.noreply.github.com>
2022-11-29 18:35:46 +0100

    Initial commit

---
+++ Assets/Script/Blade.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+    private Camera mainCamera;
+    private Collider bladeCollider;
+    private TrailRenderer bladeTrail;
+    private bool Slicing;
+
+    public Vector3 direction { get; private set; }
+    public float sliceForce = 5f;
+    public float minSliceVelocity = 0.01f;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+        bladeCollider = GetComponent<Collider>();
+        bladeTrail = GetComponentInChildren<TrailRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        StopSlicing();
+    }
+
+    private void OnDisable()
+    {
+        StopSlicing();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StarsSlicing();
+        }else if (Input.GetMouseButtonUp(0))
+        {
+            StopSlicing();
+        }else if (Slicing)
+        {
+            ContinueSlincing();
+        }
+    }
+
+    private void StarsSlicing()
+    {
+        Vector3 newPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        newPosition.z = 0f;
+
+        transform.position = newPosition;
+
+        Slicing = true;
+        bladeCollider.enabled = true;
+        bladeTrail.enabled = true;
+        bladeTrail.Clear();
+    }
+
+    private void StopSlicing()
+    {
+        Slicing = false;
+        bladeCollider.enabled = false;
+        bladeTrail.enabled = false;
+    }
+
+    private void ContinueSlincing()
+    {
+        Vector3 newPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        newPosition.z = 0f;
+
+        direction = newPosition - transform.position;
+
+        float velocity = direction.magnitude / Time.deltaTime;
+        bladeCollider.enabled = velocity > minSliceVelocity;
+
+        transform.position = newPosition;
+    }
+
+}
